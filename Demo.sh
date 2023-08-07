ff83677c006c48a26fa5189d4dcb97201a7a9d63
Saketh Kasula <saketh.kasula@snhu.edu>
2023-08-07 14:02:54 -0700

    new line added

--- Demo.sh
+++ Demo.sh
@@ -7,4 +7,4 @@ echo -e "\e[31mRED \e[32mGREEN \e[0m \e[33mYELLOW \e[34mBLUE \e[35mMAGENTA \e[36
 
 echo -e "\e[1;31mRED \e[1;4;32mGREEN\e[0m \n \e[33mYELLOW \e[34mBLUE \t \e[35mMAGENTA \e[36mCYAN \e[0m"
 
-echo -e "\e[1;31mRED \e[1;4;32mGREEN\e[0m \n\e[33mYELLOW \e[34mBLUE \t \e[35mMAGENTA \e[36mCYAN \e[0m"
+echo -e "\e[1;31mRED \e[1;4;32mGREEN\e[0m \n\e[33mYELLOW \e[34mBLUE \t \e[1;35mMAGENTA \e[36mCYAN \e[0m"
