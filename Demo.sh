341c95ab9e72fa5e304b9a5739d56915ed21b12f
Saketh Kasula <saketh.kasula@snhu.edu>
2023-08-07 13:28:28 -0700

    new line added

--- Demo.sh
+++ Demo.sh
@@ -3,4 +3,4 @@
 echo shellscripting
 echo one_more_line
 
-echo -e "\e[31mRED\e[32mGREEN\e[33mYELLOW\e[34mBLUE\e[35mMAGENTA\e[36mCYAN\e[0m"
+echo -e "\e[31mRED \e[32mGREEN \e[33mYELLOW \e[34mBLUE\e [35mMAGENTA\e[36mCYAN\e[0m"
