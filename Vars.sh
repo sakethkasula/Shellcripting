8b4e964341a4014c0067bc1b831899678c2842e0
Saketh Kasula <saketh.kasula@snhu.edu>
2023-08-07 14:21:22 -0700

    Date is being added with substitution

--- Vars.sh
+++ Vars.sh
@@ -5,6 +5,10 @@ student_name="Raju"
 echo student_name = $student_name
 echo student_name = ${student_name}
 
-Date=2023-08-07
+Date=2023-07-07
+
+echo Good Morning, Today date is $Date
+
+Date=$(date +%F)
 
 echo Good Morning, Today date is $Date
