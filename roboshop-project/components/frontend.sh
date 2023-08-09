75b80264b781fd9f4208f28c89456e1103fa8be0
Saketh Kasula <saketh.kasula@snhu.edu>
2023-08-09 14:27:31 -0700

    if else conditions being passed and

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,1 +1,39 @@
-echo installing frontend
+#The frontend is the service in RobotShop to serve the web content over Nginx.
+#
+#To Install Nginx.
+#
+#```
+## yum install nginx -y
+## systemctl enable nginx
+## systemctl start nginx
+#
+#```
+#
+#Let's download the HTDOCS content and deploy under the Nginx path.
+#
+#```
+## curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+#
+#```
+#
+#Deploy the downloaded content in Nginx Default Location.
+#
+#```
+## cd /usr/share/nginx/html
+## rm -rf *
+## unzip /tmp/frontend.zip
+## mv frontend-main/static/* .
+## mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+#
+#```
+#
+#Finally restart the service once to effect the changes.
+
+rm-f /tmp/roboshop.log
+
+echo  "installing NGINX"
+yum install nginx -y >>/tmp/roboshop.log
+
+echo "Download frontend content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>/tmp/roboshop.log
+
