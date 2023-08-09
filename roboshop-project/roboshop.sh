089792c4cfd58e33b811877190e122715efe7795
Saketh Kasula <saketh.kasula@snhu.edu>
2023-08-09 12:17:30 -0700

    if else conditions being passed and

--- roboshop-project/roboshop.sh
+++ roboshop-project/roboshop.sh
@@ -3,6 +3,6 @@
 if [ -f components/$1.sh ]; then
 bash components/$1.sh
 else
-  echo -e "/e[1;31mInvalid Input/e[0"
-  echo -e "/e[1;33mAvailable Inputs - frontend|mongodb|mysql|cart|user|payments|dispatch|redis|rabbitmq|catalogue/e[0"
+  echo -e "/e[1;31mInvalid Input/e[0m"
+  echo -e "/e[1;33mAvailable Inputs - frontend|mongodb|mysql|cart|user|payments|dispatch|redis|rabbitmq|catalogue/e[0m"
 fi
